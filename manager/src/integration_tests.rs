44406d2d10805e5e75bd373ea62957dff1182bf8
WildHashGhost-Cica <wildhashghost@gmail.com>
2022-12-04 15:49:19 +0000

    create counter and increment and reset

--- manager/src/integration_tests.rs
+++ manager/src/integration_tests.rs
@@ -166,7 +166,16 @@ mod tests {
 
     #[test]
     fn create_counter_and_increment_and_reset() {
-        unimplemented!()
+        let (mut app, manager_id, counter_id) = store_code();
+        let manager_contract = manager_instantiate(&mut app, manager_id);
+
+        instantiate_new(&mut app, &manager_contract, counter_id);
+        increment(&mut app, &manager_contract, "contract1".to_string());
+        reset(&mut app, &manager_contract, "contract1".to_string(), 10);
+
+        let res = get_contracts(&app, &manager_contract);
+        let res = get_count(&app, res.contracts[0].1.address.as_str());
+        assert_eq!(res.count, 10);
     }
 
     #[test]
